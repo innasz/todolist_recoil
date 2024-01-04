ce2d88ad35546e55c4068742fc17e31ac61611aa
innasz <mini5167@naver.com>
2024-01-04 17:51:53 +0900

    feat: useRef input focus

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,25 +1,35 @@
-import { Button, Form, Input, Space } from "antd";
-import React, { useState } from "react";
+import { Button, Input, Space } from "antd";
+import React, { useRef, useState } from "react";
 
 // 투두리스트를 투두리스트 컴포넌트에 추가해주는 컴포넌트
 const AddTodo = () => {
   const [todo, setTodo] = useState("");
-  const onChangeHandler = (e) => {
+  const inputRef = useRef(null);
+
+  function clickModifyButton() {
+    setTodo("");
+    if (inputRef.current !== null) {
+      inputRef.current.disabled = false;
+      inputRef.current.focus();
+    }
+  }
+
+  function onChangeHandler(e) {
     setTodo(e.target.value);
-    console.log("todo", todo);
-  };
-  const onSubmitHandler = (e) => {};
+  }
+
   return (
     <div className="mb-8">
       <form>
         <Space.Compact style={{ width: "100%" }}>
           <Input
+            ref={inputRef}
             type="text"
             value={todo}
             placeholder="Hey, input here :)"
             onChange={onChangeHandler}
           />
-          <Button type="default" onSubmit={onSubmitHandler}>
+          <Button type="default" onClick={clickModifyButton}>
             Add
           </Button>
         </Space.Compact>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,10 +4,10 @@ import React from "react";
 // 투두리스트를 보여주는 곳
 const TodoList = () => {
   const todoMockup = [
-    { id: 1, title: "todolist add 기능", completed: true },
-    { id: 2, title: "todolist delete 기능", completed: false },
-    { id: 3, title: "todolist check 기능", completed: true },
-    { id: 3, title: "todolist modify 기능", completed: true },
+    { id: 1, title: "todolist add 기능", checked: true },
+    { id: 2, title: "todolist delete 기능", checked: false },
+    { id: 3, title: "todolist check 기능", checked: true },
+    { id: 3, title: "todolist modify 기능", checked: true },
   ];
 
   const todoTitle = todoMockup.map((obj) => obj.title);

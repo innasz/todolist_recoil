5e224ea84400217d3d0aa1080208a42a75d70722
innasz <mini5167@naver.com>
2024-01-04 18:46:05 +0900

    feat: 추가기능 완료, title 입력할 때마다 todo 호출되는 거 잡기

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,19 +1,25 @@
-import { Button, Form, Input, Space } from "antd";
-import React, { useEffect, useRef, useState } from "react";
+import { Button, Input, Space } from "antd";
+import React, { useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 // 투두리스트를 투두리스트 컴포넌트에 추가해주는 컴포넌트
 const AddTodo = () => {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState([
+    { id: uuidv4(), title: "todolist를 만들자1", checked: false },
+    { id: uuidv4(), title: "todolist를 만들자2", checked: false },
+    { id: uuidv4(), title: "todolist를 만들자3", checked: false },
+  ]);
+  const [title, setTitle] = useState("");
   const inputRef = useRef(null);
 
-  function onChangeHandler(e) {
-    setTodo(e.target.value);
-  }
+  function onAddHandler() {
+    const newTodo = { id: uuidv4(), title: title, checked: false };
+    setTodo([...todo, newTodo]);
 
-  function onClickAddButton() {
-    setTodo("");
+    setTitle("");
     inputRef.current.focus();
   }
+  console.log("todo", todo);
 
   return (
     <div className="mb-8">
@@ -21,12 +27,12 @@ const AddTodo = () => {
         <Space.Compact style={{ width: "100%" }}>
           <Input
             ref={inputRef}
+            value={title}
             type="text"
-            value={todo}
             placeholder="Hey, input here :)"
-            onChange={onChangeHandler}
+            onChange={(e) => setTitle(e.target.value)}
           />
-          <Button type="default" onClick={onClickAddButton}>
+          <Button type="default" onClick={onAddHandler}>
             Add
           </Button>
         </Space.Compact>

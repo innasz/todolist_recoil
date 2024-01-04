6718a1ce86b0a03f5399925d81747c6d820f4c74
innasz <mini5167@naver.com>
2024-01-04 18:16:28 +0900

    feat: Input에 prop으로 ref를 줘서 해결

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,21 +1,18 @@
-import { Button, Input, Space } from "antd";
-import React, { useRef, useState } from "react";
+import { Button, Form, Input, Space } from "antd";
+import React, { useEffect, useRef, useState } from "react";
 
 // 투두리스트를 투두리스트 컴포넌트에 추가해주는 컴포넌트
 const AddTodo = () => {
   const [todo, setTodo] = useState("");
   const inputRef = useRef(null);
 
-  function clickModifyButton() {
-    setTodo("");
-    if (inputRef.current !== null) {
-      inputRef.current.disabled = false;
-      inputRef.current.focus();
-    }
-  }
-
   function onChangeHandler(e) {
     setTodo(e.target.value);
+  }
+
+  function onClickAddButton() {
+    setTodo("");
+    inputRef.current.focus();
   }
 
   return (
@@ -29,7 +26,7 @@ const AddTodo = () => {
             placeholder="Hey, input here :)"
             onChange={onChangeHandler}
           />
-          <Button type="default" onClick={clickModifyButton}>
+          <Button type="default" onClick={onClickAddButton}>
             Add
           </Button>
         </Space.Compact>
--- src/components/TodoTemplate.jsx
+++ src/components/TodoTemplate.jsx
@@ -5,8 +5,8 @@ import TodoList from "./TodoList";
 
 const TodoTemplate = () => {
   return (
-    <div className="flex w-full h-screen justify-center bg-indigo-950">
-      <div className="w-[500px] bg-white px-10 border-8 rounded-3xl border-black">
+    <div className="flex w-full h-screen justify-center items-center bg-indigo-950">
+      <div className="w-[500px] h-5/6 bg-white px-10 border-8 rounded-3xl border-black ">
         <Header />
         <AddTodo />
         <TodoList />
